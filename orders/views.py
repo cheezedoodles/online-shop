70ef450ab135e7ff82c262bcf98a2622b42b2b59
alex <hougvapes@gmail.com>
2022-09-24 18:05:48 +0300

    added a celery task to handle email notifications when ordering

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+
+from cart.cart import Cart
+
+from .tasks import order_created
 from .models import OrderItem
 from .forms import OrderCreateForm
-from cart.cart import Cart
+
 
 
 def order_create(request):
@@ -16,6 +20,8 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+            # launch async task
+            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
